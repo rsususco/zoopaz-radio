d7d394d61e69e36e3b083f57377ff372d87da374
Weldon <wjsams@gmail.com>
2013-05-30 21:30:25 -0400

    Moved authentication module into Auth class.

--- lib/auth.php
+++ lib/auth.php
@@ -2,100 +2,60 @@
 /**
  * Configuration
  */
+require_once("lib/Auth.php");
 
-$maxTrys = 5;
-
-// Set your users here.
-$users = array("wjsams@gmail.com"=>"ilovesiam", "nora.roggeveen@gmail.com"=>"pizers");
-
-// start: login
-$year = date("Y");
-$month = date("m");
-$day = date("d");
-$hour = date("H");
-$minute = date("i");
-$seconds = date("s");
-
-// Create session directories
-if (!file_exists("sessions")) {
-    mkdir("sessions");
-}
-if (!file_exists("sessions/logins")) {
-    mkdir("sessions/logins");
-}
-if (!file_exists("sessions/logins/{$year}")) {
-    mkdir("sessions/logins/{$year}");
-}
-if (!file_exists("sessions/logins/{$year}/{$month}")) {
-    mkdir("sessions/logins/{$year}/{$month}");
-}
-if (!file_exists("sessions/logins/{$year}/{$month}/{$day}")) {
-    mkdir("sessions/logins/{$year}/{$month}/{$day}");
-}
-$sessionDir = "sessions/logins/{$year}/{$month}/{$day}/{$sessid}";
-if (!file_exists($sessionDir)) {
-    mkdir($sessionDir);
-}
-
-// Session files.
-$session = "{$sessionDir}/" . $_SERVER['REMOTE_ADDR'] . ".session";
-$try = "{$sessionDir}/" . $_SERVER['REMOTE_ADDR'] . ".try";
-
-if (!file_exists($try)) {
-    file_put_contents($try, 0);
-    $trys = 0;
+if (!isset($_SESSION['auth'])) {
+    $auth = new Auth();
 } else {
-    $trys = intval(file_get_contents($try));
+    $auth = unserialize($_SESSION['auth']);
 }
 
-$disabled = "";
-$message = "";
-if ($trys > $maxTrys) {
-    $message = "<strong>Locked out.</strong><br />";
-    $disabled = "disabled='disabled'";
+if ($auth->tries > $auth->maxTries) {
+    $auth->message = "<strong>Locked out.</strong><br />";
+    $auth->disabled = "disabled='disabled'";
 }
-if ($_GET['action'] == "login" && $trys > $maxTrys) {
+
+if ($_GET['action'] == "login" && $auth->tries > $auth->maxTries) {
+    $_SESSION['auth'] = serialize($auth);
     header("Location:{$_SERVER['PHP_SELF']}");
     exit();
 }
 
 if ($_GET['action'] == "login") {
-    if (array_key_exists($_POST['username'], $users)) {
-        if ($_POST['password'] == $users[$_POST['username']]) {
+    if (array_key_exists($_POST['username'], $auth->users)) {
+        if ($_POST['password'] == $auth->users[$_POST['username']]) {
             // Set session login variables.
             if (!file_exists("sessions/users")) {
                 mkdir("sessions/users");
             }
-            $userDir = "sessions/users/{$_POST['username']}";
-            if (!file_exists($userDir)) {
-                mkdir($userDir);
+            $auth->userDir = "sessions/users/{$_POST['username']}";
+            if (!file_exists($auth->userDir)) {
+                mkdir($auth->userDir);
             }
-            $_SESSION['is_logged_in'] = true;
-            $_SESSION['username'] = $_POST['username'];
-            $_SESSION['sessionDir'] = $sessionDir;
-            $_SESSION['userDir'] = $userDir;
-            file_put_contents($session, "logged in as {$_POST['username']} on " . date("Y-m-d H:i:s"));
-            file_put_contents($try, 0);
+            $auth->username = $_POST['username'];
+            $auth->is_logged_in = true;
+            $auth->tries = 0;
+            $_SESSION['auth'] = serialize($auth);
             header("Location:{$_SERVER['PHP_SELF']}");
             exit();
         } else {
-            file_put_contents($try, $trys + 1);
+            $auth->tries = $auth->tries + 1;
         }
     } else {
-        file_put_contents($try, $trys + 1);
+        $auth->tries = $auth->tries + 1;
     }
 }
 
-if (!file_exists($session) || $trys > $maxTrys) {
-    print($message);
+if (!$auth->is_logged_in || $auth->tries > $auth->maxTries) {
+    $_SESSION['auth'] = serialize($auth);
+    print($auth->message);
     print <<<eof
 <form action="{$_SERVER['PHP_SELF']}?action=login" method="post">
-username: <input {$disabled} type="text" name="username" id="username" /><br />
-password: <input {$disabled} type="password" name="password" id="password" /><br />
-<input {$disabled} type="submit" value="login" />
+username: <input {$auth->disabled} type="text" name="username" id="username" /><br />
+password: <input {$auth->disabled} type="password" name="password" id="password" /><br />
+<input {$auth->disabled} type="submit" value="login" />
 </form>
 eof;
     die();
 }
 // end: login
-
